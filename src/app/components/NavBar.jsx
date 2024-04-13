fab6c6aa05ed99630aceab21ae9fa0949a1a7dca
diegosierra <diegosierra@cityciudad.com>
2024-04-13 07:13:31 -0500

    version base

---
+++ src/app/components/NavBar.jsx
@@ -0,0 +1,10 @@
+
+function NavBar() {
+  return (
+    <header className="fixed h-20 bg-white">
+      Menu
+    </header>
+  )
+}
+
+export default NavBar
---
+++ src/app/ui/fonts.js
@@ -0,0 +1,5 @@
+import { Questrial } from "next/font/google";
+import {Russo_One} from "next/font/google";
+
+export const questrial = Questrial({ subsets: ["latin"], weight: ["400"] });
+export const russo = Russo_One({ subsets: ["latin"] });
